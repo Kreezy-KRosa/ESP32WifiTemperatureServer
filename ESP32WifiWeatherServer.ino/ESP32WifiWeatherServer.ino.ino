1e5d4cbd75384ba42e7bbd0f4b27f148ad0daf08
Kreezy <krosa195@yahoo.com>
2018-01-30 13:23:27 -0500

    Finalized

---
+++ ESP32WifiWeatherServer.ino/ESP32WifiWeatherServer.ino.ino
@@ -0,0 +1,72 @@
+// ESP32 Wifi Temperature Server with Remote Client via Web Browser
+// By Kreezy(KRosa) 1.30.2018
+
+// This code is public domain 
+
+
+#include "DHTesp.h"                 // Install these libraries if you don't already have them
+#include "ESP32Ticker.h"
+#include <WiFi.h>
+
+DHTesp dht;               
+
+int dhtPin = 4;                        // DHT-11 sensor data wire to Pin D4
+
+
+
+const char* ssid = "SSID";             // enter your wifi's ssid name inside the ""'s // EXAMPLE "MyWifi"
+
+const char* password = "PASSWORD";     // enter your wifi's password inside the ""'s  // Example "Password"
+
+int ledPin = 2;                        // declaring the onboard LED on Pin D2
+int clientLed = 5;                     // declaring the external LED on Pin 5
+
+WiFiServer server(80);                // wifi server setup on port 80
+
+void setup()
+{ 
+  Serial.begin(9600);
+  dht.setup(dhtPin, DHTesp::DHT11);  // for DHTesp:: enter sensor type (DHT11, DHT22, etc)
+  
+  pinMode(ledPin,OUTPUT);            // making the onboard LED pin an output
+  pinMode(clientLed,OUTPUT);         // making the external LED pin an output 
+  
+  digitalWrite(clientLed,LOW);
+
+  WiFi.begin(ssid,password);                   // this is when we try to connect to your network
+  while ((!(WiFi.status() == WL_CONNECTED))){
+  delay(300);
+  digitalWrite(ledPin,HIGH);                   // this makes the onboard led blink until connected
+  delay(50);
+  digitalWrite(ledPin,LOW);
+  delay(50);
+  }
+  Serial.println("Connected");                 // once connected to a network, the onboard led will stay on
+  Serial.println((WiFi.localIP()));
+  server.begin();
+  digitalWrite(ledPin,HIGH);
+  }
+
+void loop() {
+  
+  WiFiClient client = server.available();               // if your connected device is available this will happen
+    if (!client) { return; }
+    while(!client.available()){  delay(1); }
+    float newTempValue = dht.getTemperature();          // reads the sensor
+    int fahrenValue = (newTempValue * 1.8 + 32);        // converts the reading the Degrees Farhenheit
+    digitalWrite(clientLed,HIGH);                       // external led will light when this info is being served to client
+    Serial.println("Client Connected");
+    client.println("HTTP/1.1 200 OK");
+    client.println("Content-Type: text/html");
+    client.println();
+    client.println("<!DOCTYPE HTML>");
+    client.println("<html>");   
+    client.print("Temperature: ");                       // this is where the text on the webpage served is created
+    client.print(fahrenValue);                           // this variable will hold the temperature data
+    client.print(" Degrees Fahrenheit");
+    client.println("</html>");
+    client.println("<meta http-equiv=\"refresh\" content=\"5\">");  // this line will refresh the webpage every 5 seconds
+    client.stop();                                     // to change the refresh rate, change the 5 to any desired number of seconds 
+    digitalWrite(clientLed,LOW);                                    // turns the external led off when finishing serving client
+    Serial.println("Client Disconnected");                          // prints this line in the serial monitor for debugging
+}  
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # ESP32WifiTemperatureServer
+# By Kreezy(KRosa) 1.30.2018
+
+This is an example of a ESP32 Wifi temperature server using a DHT-11 sensor.
+
+Load the code, enter your SSID and password where needed, upload than check the serial monitor
+Take the IP it gives you and enter that into a web browser
+The current temperature reading should show up on the webpage served
+The webpage is set to refresh itself every 5 seconds
+
+If your onboard LED is not a solid blue than you entered your SSID info wrong
+
+If the ESP32 does connect to a network, the onboard LED will stay lit until disconnected
